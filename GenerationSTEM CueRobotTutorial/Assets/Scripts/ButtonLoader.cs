70020c87cd86c4d4e32da57064357d4b32dcb3eb
Stephanie <stephcarpin710@gmail.com>
2020-03-08 22:49:06 -0700

    button controls

---
+++ GenerationSTEM CueRobotTutorial/Assets/Scripts/ButtonLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLoader : MonoBehaviour
+{
+    public GameObject Action;
+    public GameObject Control;
+
+    public void ActionButton()
+    {
+        Control.gameObject.SetActive(false);
+        Action.gameObject.SetActive(true);
+    }
+
+    public void ControlButton()
+    {
+        Action.gameObject.SetActive(false);
+        Control.gameObject.SetActive(true);
+    }
+
+    void OnMouseDown()
+    {
+        Action.gameObject.SetActive(false);
+        Control.gameObject.SetActive(false);
+    }
+
+}
